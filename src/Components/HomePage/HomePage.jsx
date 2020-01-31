255b99517e206c0b4496171c2f2b5e1c48e8e513
Hélèna Thomassin <thomassin.helena@gmail.com>
2020-01-31 07:59:50 +0100

    delete image in HomePage and add map

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Map from './Map';
 
-
 const HomePage = () => {
   return (
-    <div id="img">       
-    <a target="_blank"> mentions légales</a>
-       <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Sally_of_the_Sawdust_%28film_poster%29.jpg" width="50%"></img>
+    <div>       
+     <Map {...Map.defaultProps}/>
     </div> 
   )
 }
